eb2a567fada81d6ebabe8da6960b374668a2e771
jtungate1 <tungate.3@wright.edu>
2026-09-21 15:06:29 -0400

    Encapsulate red circle detection in fileSort function
    Refactored code to encapsulate functionality in fileSort function.

--- src/Sorter.py
+++ src/Sorter.py
@@ -1,16 +1,12 @@
-from pathlib import Path
 from RefinedRedCircles import detect_red_circles as detect
 import shutil
 
-# Specify the directory path
-directory_path = Path("images/")
-destination_path = Path("detected_circles/")
-
-# Loop through items and filter for files only
-for file_path in directory_path.iterdir():
-    if detect(file_path):
-        print(f"Red circles detected in: {file_path.name}")
-        # Move the file to the destination directory
-        shutil.copy(file_path, destination_path / file_path.name)
-
-
+def fileSort(directory_path, destination_path):
+    # Loop through items and filter for files only
+    for file_path in directory_path.iterdir():
+        if detect(file_path):
+            print(f"Red circles detected in: {file_path.name}")
+            # Move the file to the destination directory
+            shutil.copy(file_path, destination_path / file_path.name)
+    
+    
